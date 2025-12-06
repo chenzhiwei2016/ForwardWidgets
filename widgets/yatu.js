4a7bc328cf7e1a1de2af6ac9aca47bc23c90ce07
不凉少年 <2895509692@qq.com>
2025-12-07 00:32:01 +0800

    Change author name from 'huangxd' to 'czw'

--- widgets/yatu.js
+++ widgets/yatu.js
@@ -199,7 +199,7 @@ WidgetMetadata = {
     version: "1.0.6",
     requiredVersion: "0.0.1",
     description: "解析雅图每日放送更新以及各类排行榜【五折码：CHEAP.5;七折码：CHEAP】",
-    author: "huangxd",
+    author: "czw",
     site: "https://github.com/chenzhiwei2016/ForwardWidgets"
 };
 
