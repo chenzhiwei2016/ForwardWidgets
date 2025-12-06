d6af7eafbf70fda7e2959f3f86529d5b9385c4aa
不凉少年 <2895509692@qq.com>
2025-12-07 00:32:20 +0800

    Update author name in zhuijurili.js

--- widgets/zhuijurili.js
+++ widgets/zhuijurili.js
@@ -251,7 +251,7 @@ WidgetMetadata = {
     version: "1.0.4",
     requiredVersion: "0.0.1",
     description: "解析追剧日历今/明日播出剧集/番剧/国漫/综艺、周历、各项榜单、今日推荐等【五折码：CHEAP.5;七折码：CHEAP】",
-    author: "huangxd",
+    author: "czw",
     site: "https://github.com/chenzhiwei2016/ForwardWidgets"
 };
 
