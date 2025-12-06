5d08b7186e760dcea14b46a20a4ac6bb609ee598
不凉少年 <2895509692@qq.com>
2025-12-07 00:02:17 +0800

    Update site URL in danmu_auto.js

--- widgets/danmu_auto.js
+++ widgets/danmu_auto.js
@@ -19,7 +19,7 @@ WidgetMetadata = {
   requiredVersion: "0.0.2",
   description: "自动获取播放链接并从服务器获取弹幕【五折码：CHEAP.5;七折码：CHEAP】",
   author: "huangxd",
-  site: "https://github.com/huangxd-/ForwardWidgets",
+  site: "https://github.com/chenzhiwei2016/ForwardWidgets",
   globalParams: [
     {
       name: "other_server",
