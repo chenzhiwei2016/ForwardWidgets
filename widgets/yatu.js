1a970885c0151db1a3056b216f221ede9970fe0a
不凉少年 <2895509692@qq.com>
2025-12-07 00:03:40 +0800

    Update GitHub links in yatu.js

--- widgets/yatu.js
+++ widgets/yatu.js
@@ -200,7 +200,7 @@ WidgetMetadata = {
     requiredVersion: "0.0.1",
     description: "解析雅图每日放送更新以及各类排行榜【五折码：CHEAP.5;七折码：CHEAP】",
     author: "huangxd",
-    site: "https://github.com/huangxd-/ForwardWidgets"
+    site: "https://github.com/chenzhiwei2016/ForwardWidgets"
 };
 
 // 基础获取TMDB数据方法
@@ -340,7 +340,7 @@ async function loadLatestItems(params = {}) {
             sin3: 'tv',
         };
 
-        const response = await Widget.http.get("https://gist.githubusercontent.com/huangxd-/28a30eac8051ccb05a43c6f49a117286/raw/zuijin.asp", {
+        const response = await Widget.http.get("https://gist.githubusercontent.com/chenzhiwei2016/28a30eac8051ccb05a43c6f49a117286/raw/zuijin.asp", {
             headers: {
                 "User-Agent":
                     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
@@ -404,7 +404,7 @@ function getClickItemInfos(data, typ) {
 }
 
 async function fetchFinalItems(genre, typ, mediaTypeDict, suffixDict) {
-    const response = await Widget.http.get(`https://gist.githubusercontent.com/huangxd-/28a30eac8051ccb05a43c6f49a117286/raw/${genre}.${suffixDict[genre]}`, {
+    const response = await Widget.http.get(`https://gist.githubusercontent.com/chenzhiwei2016/28a30eac8051ccb05a43c6f49a117286/raw/${genre}.${suffixDict[genre]}`, {
         headers: {
             "User-Agent":
                 "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
